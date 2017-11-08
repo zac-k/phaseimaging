37530c3494d4407ddd528f33e550557df7904f93
zac <zac.kemp@outlook.com>
2017-11-09 07:22:18 +1100

    corrected mistakes in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='phase-imaging',
+setup(name='phaseimaging',
       version='0.1',
       description='Library of functions for phase imaging',
-      url='http://github.com/zac-k/phase-imaging',
+      url='http://github.com/zac-k/phaseimaging',
       author='Zachary DC Kemp',
       author_email='zachary.kemp@monash.edu',
       license='GPLv3',
-      packages=['phase-imaging'])
+      packages=['phaseimaging'])

96f2c428d5ea16550b1a0a3c82d82afdadf378dc
zac <zac.kemp@outlook.com>
2017-11-29 10:27:03 +1100

    Added docstrings for every existing function.

--- phaseimaging/plot.py
+++ phaseimaging/plot.py
@@ -5,24 +5,57 @@ import warnings
 
 
 def plot_image(image, limits=None, title=None):
+    """
+    Plot an image array on the screen.
 
+    Args:
+        image (ndarray): The image data to plot.
+        limits (sequence of floats|optional): Two-element sequence providing the minimum
+                                              and maximum values of the plot. Values outside
+                                              this range will be clipped at the min/max. If
+                                              limits are not provided, the color axis will be
+                                              scaled according to the max/min values in the
+                                              image array.
+        title (string|optional): The title to display on the figure. Default is None.
+
+    Returns:
+        None
+    """
+
+    # If the image is complex, warn the user and plot only the real part.
     if np.iscomplexobj(image):
         warnings.warn('Image is complex; only displaying real part')
         image = np.real(image)
+
+    # Plot the scaled image if no limits are provided.
     if limits is None:
         plt.imshow(image, cmap='gray')
+    # Otherwise, plot the image using the limits.
     else:
         vmin = limits[0]
         vmax = limits[1]
         plt.imshow(image, cmap='gray', vmin=vmin, vmax=vmax)
-    if title is None:
-        title = ""
-    plt.title(title)
+    # Add the title if one is provided.
+    if title is not None:
+        plt.title(title)
     plt.show(block=True)
 
 
 def plot_quiver(vec_field, width=(1, 1, 1), title=None):
+    """
+    Plot a three-dimensional quiver plot on screen.
 
+    Args:
+        vec_field (ndarray): The vector field to plot.
+        width (sequence of floats|optional): The dimensions of the field in units of length.
+                                             Default is the unit cube.
+        title (string|optional): Title to display on the plot. Default is None.
+
+    Returns:
+        None
+    """
+
+    # Generate a figure with 3D axes.
     fig = plt.figure()
     ax = fig.gca(projection='3d')
 
@@ -37,29 +70,49 @@ def plot_quiver(vec_field, width=(1, 1, 1), title=None):
     ax.quiver(x, y, z, vec_field[:, :, :, 0], vec_field[:, :, :, 1], vec_field[:, :, :, 2],
               length=0.01 * np.linalg.norm(width), normalize=True)
 
-    if title is None:
-        title = ""
-    plt.title(title)
+    # Add the title if one is provided.
+    if title is not None:
+        plt.title(title)
     plt.show(block=True)
 
 
 def save_image(image, output_path, limits=None):
+    """
+    Save an image array in an image format. This produces a clean image with no axes or border.
+    Args:
+        image (ndarray): The image data to save.
+        output_path (string): The path of file to save.
+        limits (sequence of floats|optional): Two-element sequence providing the minimum
+                                              and maximum values of the image. Values outside
+                                              this range will be clipped at the min/max. If
+                                              limits are not provided, the color axis will be
+                                              scaled according to the max/min values in the
+                                              image array.
 
+    Returns:
+        None
+    """
+
+    # Generate a figure with no axes, border, etc.
     fig = plt.figure()
     fig.set_size_inches(1, 1)
     ax = plt.Axes(fig, [0, 0, 1, 1])
     ax.set_axis_off()
     fig.add_axes(ax)
+
+    # If the image is complex, warn the user and discard the imaginary part.
     if np.iscomplexobj(image):
         warnings.warn('Image is complex; only saving real part')
         image = np.real(image)
+
+    # Plot the image, scaled according to the limits if they are provided.
     if limits is None:
         ax.imshow(image, cmap='gray', aspect='auto', interpolation='none')
     else:
         vmin = limits[0]
         vmax = limits[1]
         ax.imshow(image, cmap='gray', vmin=vmin, vmax=vmax, aspect='auto', interpolation='none')
-    # Remove tick marks from plot
 
+    # Save the figure with a high resolution, and close it.
     plt.savefig(output_path, dpi=800)
     plt.close()
